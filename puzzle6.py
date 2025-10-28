cb3ff9ff9ded024844319fac4a6f4293a4cde00a
Peter Varchol <peter.varchol@plaut.sk>
2025-10-28 14:53:13 +0100

    Puzzle6 - first try

---
+++ puzzle6.py
@@ -0,0 +1,10 @@
+with open("resources/Puzzle6.txt") as puzzle_input_file:
+    pass
+
+
+
+print(
+    "religiÃ«n".encode("latin-1").decode("utf-8"),
+    "kÃ¼rst".encode("latin-1").decode("utf-8"),
+    "pugilarÃÂ£o".encode("latin-1").decode("utf-8").encode("latin-1").decode("utf-8")
+)
