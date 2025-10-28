8cf11661cf3930b9266ba57af38a5faea7ff5977
Peter Varchol <peter.varchol@plaut.sk>
2025-10-28 13:11:13 +0100

    Puzzle5

--- puzzle5.py
+++ puzzle5.py
@@ -1,16 +1,9 @@
-from datetime import datetime
-from zoneinfo import ZoneInfo
-import re
+with open("resources/Puzzle5.txt") as puzzle_input_file:
+    coordinate = 0
+    shitPiles = 0
 
-with (open("resources/Puzzle4.txt") as puzzle_input_file):
-    p = re.compile(r"(Departure|Arrival):\s+(\S+)\s+(.+)")
-
-    matches = [(m.group(2), m.group(3)) for m in \
-        [p.match(sl) for sl in \
-            [line.strip() for line in puzzle_input_file]] if m]
-
-    dates = [datetime.strptime(m[1], "%b %d, %Y, %H:%M").replace(tzinfo=ZoneInfo(m[0])) for m in matches]
-
-    print(
-        sum(int((dates[i + 1] - dates[i]).total_seconds()/60) for i in range(0, len(dates), 2))
-    )
+    for line in [line.rstrip("\n") for line in puzzle_input_file]:
+        if line[coordinate % len(line)] == '💩':
+            shitPiles += 1
+        coordinate = coordinate + 2
+    print(shitPiles)
